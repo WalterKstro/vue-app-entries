9a98c7b52d85de641c935ad1549cffc4062e3a3d
Walter Castro <walterkstro@gmail.com>
2022-03-05 11:14:28 +0100

    Resuelto, problema del store local, en modulos

--- src/modules/daybook/store/index.js
+++ src/modules/daybook/store/index.js
@@ -1,15 +1,14 @@
-import { createStore } from 'vuex'
 import actions from './actions'
 import getters from './getters'
 import mutations from './mutations'
 import state from './state'
 
-const storeDayBook = createStore({
+const storeDayBook = {
   namespaced: true,
   state,
   mutations,
   actions,
   getters
-})
+}
 
 export default storeDayBook
